2b1ece43370879fcbbf53b72956fa8268ee44881
lkorth <luke@lukekorth.com>
2012-11-27 09:48:06 -0600

    Add method to detect if the app is in debug mode

--- src/com/lukekorth/android_http/HttpHelper.java
+++ src/com/lukekorth/android_http/HttpHelper.java
@@ -1,5 +1,8 @@
 package com.lukekorth.android_http;
 import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.util.Log;
 
 import com.google.gson.Gson;
@@ -11,6 +14,8 @@ import java.io.IOException;
 public class HttpHelper {
 
     private static Gson gson = null;
+    private Context mContext;
+    private boolean DEBUG_HTTP;
 
     public HttpHelper(Context context) {
         new HttpHelper(context, (long) 10 * 1024 * 1024); // 10 MiB
@@ -29,6 +34,22 @@ public class HttpHelper {
             gson = new Gson();
 
         return gson;
+    }
+
+
+    // http://izvornikod.com/Blog/tabid/82/EntryId/13/How-to-check-if-your-android-application-is-running-in-debug-or-release-mode.aspx
+    private boolean IsDebug() {
+        boolean debuggable = false;
+
+        PackageManager pm = mContext.getPackageManager();
+        try {
+            ApplicationInfo appinfo = pm.getApplicationInfo(mContext.getPackageName(), 0);
+            debuggable = (0 != (appinfo.flags &= ApplicationInfo.FLAG_DEBUGGABLE));
+        } catch (NameNotFoundException e) {
+            /* debuggable variable will remain false */
+        }
+
+        return debuggable;
     }
 
 }
